c33b7086046f06b45882722293af55102d710240
Roman Onyshkiv <roman.onushkiv@gmail.com>
2023-10-07 15:36:19 +0300

    Lab Level 2 - v1.0. Blockers - w Determinant and Inverse implementation

--- Program.cs
+++ Program.cs
@@ -38,8 +38,20 @@ namespace MOVD_Lab2
             // 5) Транспонування матриці
             //TransposeMatrix(matrixA);
 
+            // 6)Операція порівняння матриць
+            //bool areEqual = CompareMatrices(matrixA);
+
+            //if (areEqual)
+            //{
+            //    Console.WriteLine("Matrices are equal because all their corresponding elements are also equal.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Matrices are not equal !");
+            //}
+
             //TODO
-            //6) Реалізувати знаходження визначника матриці 
+            //7) Реалізувати знаходження визначника матриці 
             //CalculateDeterminant(); 
 
             //TODO
@@ -48,6 +60,8 @@ namespace MOVD_Lab2
             //------------------------------------------------------------------------
             //Другий рівень : 
             // 1) Розв’язати СЛАР, використовуючи метод Крамера
+            double detA = CalculateDeterminant();
+            CramerMethod(detA);
         }
 
         static void MultiplyMatrixByNumber(int[,] matrix)
@@ -265,7 +279,49 @@ namespace MOVD_Lab2
 
         }
 
-        static void CalculateDeterminant()
+        static bool CompareMatrices(int[,] mainMatrix)
+        {
+            int rows = 5;
+            int col = 5;
+            Random randomMatrixElemenents = new Random();
+            int[,] newMatrix = new int[rows, col];
+
+            // Заповнуюємо матрицю рандомними числами (randomMatrixElemenents < 20)
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    int randomNumber = randomMatrixElemenents.Next(20);
+                    newMatrix[i, j] = randomNumber;
+                }
+            }
+
+            // Виводимо матрицю на екран
+            Console.WriteLine("Matrix we will use for Comprasion operation : ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(newMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (mainMatrix[i, j] != newMatrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+
+        static double CalculateDeterminant()
         {
             var matrix = Matrix<double>.Build.DenseOfArray(new double[,]
             {
@@ -279,6 +335,8 @@ namespace MOVD_Lab2
             double determinant = matrix.Determinant();
 
             Console.WriteLine($"Determinant of the 5x5 matrix: {determinant}");
+
+            return determinant;
         }
 
         static void InverseMatrix()
@@ -315,6 +373,59 @@ namespace MOVD_Lab2
 
         }
 
+        //------------------------------------------------------------------------
+        //Другий рівень : 
+        static void CramerMethod(double detA)
+        {
+            double[,] a = new double[5, 5];
+            double[] b = new double[5];
 
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Enter the coefficients of the equation {i + 1}:");
+                string[] coefficients = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < 5; j++)
+                {
+                    a[i, j] = Convert.ToDouble(coefficients[j]);
+                }
+
+                Console.Write("Enter free members : ");
+                b[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nThe system of equations was successfully entered \n!");
+
+            Matrix<double> matrixA = Matrix<double>.Build.DenseOfArray(a);
+            Vector<double> vectorB = Vector<double>.Build.Dense(b);
+
+            if (detA != 0)
+            {
+                Console.WriteLine($"Matrix A determinant =  {detA}");
+            }
+            else
+            {
+                Console.WriteLine("Матриця A вироджена");
+            }
+
+            Console.WriteLine("Cramer Method: ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"\nMatrix A{i + 1}");
+
+                Matrix<double> matrixAi = matrixA.Clone();
+                matrixAi.SetColumn(i, vectorB);
+
+                double detAi = matrixAi.Determinant();
+                double xi = detAi / detA;
+
+                Console.WriteLine($"\nMatrix А determinant{i + 1}: {detAi}");
+                Console.WriteLine($"x{i + 1} = {xi}");
+            }
+        }
+
+
+        
     }
 }
